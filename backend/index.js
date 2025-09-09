503e1e27009aff02bb8155eba071d12c2a2894d3
Ankit Mishra <ankitmishra.akmishra.@gmail.com>
2025-09-09 11:54:58 +0530

    #

--- backend/index.js
+++ backend/index.js
@@ -6,8 +6,11 @@ import messageRouter from './route/messageroute.js';
 import cookieParser from 'cookie-parser';
 import userRouter from './route/userRoute.js'
 import cors from "cors";
+import path from "path";
 
 import { app, server } from './Socket/socket.js'
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -27,6 +30,12 @@ app.use(cors({
 app.use('/api/auth', authRouter)
 app.use('/api/message', messageRouter)
 app.use('/api/user', userRouter)
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+})
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
@@ -34,4 +43,38 @@ server.listen(PORT, () => {
     console.log(`server is running at ${PORT}`);
 })
 
+// import express from 'express'
+// import dotenv from 'dotenv'
+// import dbconnect from './DB/dbconnect.js';
+// import authRouter from './route/authUser.js';
+// import messageRouter from './route/messageroute.js';
+// import cookieParser from 'cookie-parser';
+// import userRouter from './route/userRoute.js'
+// import cors from "cors";
 
+// import { app, server } from './Socket/socket.js'
+
+// dotenv.config();
+
+// dbconnect();
+// app.get('/', (req, res) => {
+//     res.send('hello');
+// })
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(cookieParser());
+// app.use(cors({
+//     origin: "http://localhost:3000",  // frontend URL
+//     credentials: true
+// }));
+
+// app.use('/api/auth', authRouter)
+// app.use('/api/message', messageRouter)
+// app.use('/api/user', userRouter)
+// const PORT = process.env.PORT || 3000;
+
+// server.listen(PORT, () => {
+
+//     console.log(`server is running at ${PORT}`);
+// })
